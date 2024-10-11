29981c3754a6ad3c0727cfcb754c025dd2b7d74d
KusumeMotohisa <link080514@gmail.com>
2024-10-12 01:22:27 +0900

    ピースの爆発の処理を修正

---
+++ Assets/KusumeFile/Scripts/Outsiders/PieceLedger.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Kusume
+{
+    [Serializable]
+    public struct ColorInfo
+    {
+        //TODO:Change type to enum.
+        public PieceTag tag;
+        public Color32 color;
+        public float ratio;
+    }
+    [Serializable]
+    public struct SizeInfo
+    {
+        public float size;
+        public float power;
+        public float ratio;
+    }
+    public struct PieceInfo
+    {
+        public ColorInfo color;
+        public SizeInfo size;
+    }
+
+    [CreateAssetMenu(fileName = "PieceLedger", menuName = "ScriptableObjects/PieceLedger", order = 1)]
+    public class PieceLedger : ScriptableObject
+    {
+        [SerializeField]
+        private ColorInfo[] colors;
+        [SerializeField]
+        private SizeInfo[] sizes;
+        private float sum_color = 0;
+        private float sum_size = 0;
+
+        private Unity.Mathematics.Random random = new Unity.Mathematics.Random();
+
+        public void Setup()
+        {
+            random = new Unity.Mathematics.Random((uint)System.DateTime.Now.Ticks);
+        }
+
+        private void Initialize()
+        {
+            sum_color = 0;
+            sum_size = 0;
+            foreach (ColorInfo color in colors)
+            {
+                sum_color += color.ratio;
+            }
+            foreach (SizeInfo size in sizes)
+            {
+                sum_size += size.ratio;
+            }
+        }
+
+        public PieceInfo GetRandomPiece()
+        {
+            if (sum_color <= 0)
+            {
+                Initialize();
+            }
+            PieceInfo info = new PieceInfo();
+            int colorIndex = GetRandomColor();
+            if (colorIndex < 0)
+            {
+                return info;
+            }
+            int sizeIndex = GetRandomSize();
+            if (sizeIndex < 0)
+            {
+                return info;
+            }
+            info.color = colors[colorIndex];
+            info.size = sizes[sizeIndex];
+            return info;
+        }
+        public int GetRandomColor()
+        {
+            float randomized = random.NextFloat() * sum_color;
+            for (int i = 0; i < colors.Length; i++)
+            {
+                randomized -= colors[i].ratio;
+                if (randomized <= 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public int GetRandomSize()
+        {
+            float randomized = random.NextFloat() * sum_size;
+            for (int i = 0; i < sizes.Length; i++)
+            {
+                randomized -= sizes[i].ratio;
+                if (randomized <= 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+
+}
+
--- Assets/KusumeFile/Scripts/Piece/Create/CreatePiece.cs
+++ Assets/KusumeFile/Scripts/Piece/Create/CreatePiece.cs
@@ -1,140 +1,124 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// ピースのタグ
-/// </summary>
-public enum PieceTag
-{
-    Red,
-    Blue,
-    Green,
-    Yellow,
-    Pink,
-    Purple,
-    Count
-}
-
-/// <summary>
-/// ピースを生成するクラス
-/// </summary>
-public class CreatePiece : MonoBehaviour
+namespace Kusume
 {
     /// <summary>
-    /// ピースを生成するオブジェクト関係の変数
+    /// ピースのタグ
     /// </summary>
-    [SerializeField]
-    private List<Transform> pieceSpawnPosition = new List<Transform>();
-
-    private int creatorCount = 0;
-
-    private int currentCreatorPositionCount = 0;
-
-    [Header("ピースオブジェクトを格納したScriptableObject")]
-    [SerializeField]
-    private PieceDataList pieceData = null;
-
-    [SerializeField]
-    private GameObject basePiece = null;
-
-    [Header("ピースごとの最大カウント")]
-    [SerializeField]
-    private int maxPieceCount = 100;
-
-    private static int currentPieceCount = 0;
-    public static int CurrentPieceCount { get { return currentPieceCount; }set { currentPieceCount = value; } }
-    /// <summary>
-    /// ピースの親オブジェクト
-    /// </summary>
-    [SerializeField]
-    private GameObject piecesParent = null;
-    /// <summary>
-    /// ピース生成の間隔をとる変数
-    /// </summary>
-    private float createCoolDown = 0f;
-
-    private void Awake()
+    public enum PieceTag
     {
-        GameObject g = null;
-        for(int i = 0; i < transform.childCount; i++)
+        Red,
+        Blue,
+        Green,
+        Yellow,
+        Pink,
+        Purple,
+        Count
+    }
+
+    /// <summary>
+    /// ピースを生成するクラス
+    /// </summary>
+    public class CreatePiece : MonoBehaviour
+    {
+        /// <summary>
+        /// ピースを生成するオブジェクト関係の変数
+        /// </summary>
+        [SerializeField]
+        private List<Transform> pieceSpawnPosition = new List<Transform>();
+
+        private int creatorCount = 0;
+
+        private int currentCreatorPositionCount = 0;
+
+        [Header("ピースオブジェクトを格納したScriptableObject")]
+        [SerializeField]
+        private PieceDataList pieceData = null;
+
+        [SerializeField]
+        private PieceLedger pieceLedger = null;
+
+        [SerializeField]
+        private GameObject basePiece = null;
+
+        [Header("ピースごとの最大カウント")]
+        [SerializeField]
+        private int maxPieceCount = 100;
+
+        private static int currentPieceCount = 0;
+        public static int CurrentPieceCount { get { return currentPieceCount; }set { currentPieceCount = value; } }
+        /// <summary>
+        /// ピースの親オブジェクト
+        /// </summary>
+        [SerializeField]
+        private GameObject piecesParent = null;
+        /// <summary>
+        /// ピース生成の間隔をとる変数
+        /// </summary>
+        private float createCoolDown = 0f;
+
+        private void Awake()
         {
-            g = transform.GetChild(i).gameObject;
-            pieceSpawnPosition.Add(g.transform);
+            GameObject g = null;
+            for(int i = 0; i < transform.childCount; i++)
+            {
+                g = transform.GetChild(i).gameObject;
+                pieceSpawnPosition.Add(g.transform);
+            }
+            creatorCount = transform.childCount;
         }
-        creatorCount = transform.childCount;
-    }
 
-    void Update()
-    {
-        if(createCoolDown >= 0)
+        private void Start()
         {
-            createCoolDown -= Time.deltaTime;
-            return;
+            pieceLedger.Setup();
         }
-        Create();
-    }
 
-    private enum ScaleTag
-    {
-        Lv0,
-        Lv1,
-        DataEnd
-    }
+        void Update()
+        {
+            if(createCoolDown >= 0)
+            {
+                createCoolDown -= Time.deltaTime;
+                return;
+            }
+            Create();
+        }
 
-    /// <summary>
-    /// Updateで呼び出し
-    /// </summary>
-    private void Create()
-    {
-        if(currentPieceCount >= maxPieceCount) { return; }
+        /// <summary>
+        /// Updateで呼び出し
+        /// </summary>
+        private void Create()
+        {
+            if(currentPieceCount >= maxPieceCount) { return; }
         
-        //ピースの生成
-        GameObject p = Instantiate(basePiece, pieceSpawnPosition[currentCreatorPositionCount].position,Quaternion.identity);
-        Rigidbody2D rb = p.GetComponent<Rigidbody2D>();
-        rb.AddForce(Vector2.up * 25.0f, ForceMode2D.Impulse);
+            //ピースの生成
+            GameObject p = Instantiate(basePiece, pieceSpawnPosition[currentCreatorPositionCount].position,Quaternion.identity);
+            Rigidbody2D rb = p.GetComponent<Rigidbody2D>();
+            rb.AddForce(Vector2.up * 25.0f, ForceMode2D.Impulse);
 
-        int tagNum = Random.Range((int)ScaleTag.Lv0, (int)ScaleTag.DataEnd);
+            var pieceInfo = pieceLedger.GetRandomPiece();
 
-        ScaleTag tag = (ScaleTag)tagNum;
-        float scale = 1.0f;
-        switch (tag)
-        {
-            case ScaleTag.Lv0:
-                scale = 1.25f;
-                break;
-            case ScaleTag.Lv1:
-                scale = 1.5f;
-                break;
-        }
+            float scale = pieceInfo.size.size;
 
-        p.transform.localScale *= scale;
-        //ピースの親を設定
-        p.transform.SetParent(piecesParent.transform);
+            p.transform.localScale *= scale;
+            //ピースの親を設定
+            p.transform.SetParent(piecesParent.transform);
         
-        Piece onePiece = p.GetComponent<Piece>();
+            Piece onePiece = p.GetComponent<Piece>();
 
-        PieceData data = pieceData.PieceDatas[PieceRandomNumber()];
-        onePiece.SetPieceData(data);
+            onePiece.SetPieceData(pieceInfo.color);
 
-        //生成位置を変更
-        currentCreatorPositionCount++;
-        //生成位置の最大値に達したら
-        if (currentCreatorPositionCount >= creatorCount)
-        {
-            //0に
-            currentCreatorPositionCount = 0;
+            //生成位置を変更
+            currentCreatorPositionCount++;
+            currentPieceCount++;
+            //生成位置の最大値に達したら
+            if (currentCreatorPositionCount >= creatorCount)
+            {
+                //0に
+                currentCreatorPositionCount = 0;
+            }
+            //生成のクールダウン
+            createCoolDown = 0;
         }
-        //生成のクールダウン
-        createCoolDown = 0.1f;
-    }
-
-    private int PieceRandomNumber()
-    {
-        int num = 0;
-        int min = (int)PieceTag.Red;
-        int max = (int)PieceTag.Count;
-        num = Random.Range(min, max);
-        currentPieceCount++;
-        return num;
     }
 }
