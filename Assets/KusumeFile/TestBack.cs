341e0a93ccc8b6b3940039d73a2de2d891f79a5b
KusumeMotohisa <link080514@gmail.com>
2024-10-18 16:16:56 +0900

    円上に画面フェードアイン・アウトのテスト処理を作成

---
+++ Assets/KusumeFile/TestBack.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestBack : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform circle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            circle.sizeDelta += Vector2.one * 100 * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            circle.sizeDelta -= Vector2.one * 100 * Time.deltaTime;
+
+        }
+    }
+}
