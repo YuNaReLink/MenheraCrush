394ea205252ba6628e8faf661b7e833b41741557
LucKee-the-Murderer <murderyy@gmail.com>
2024-12-11 14:47:43 +0900

    I don't know why, but it's successful.

--- Assets/Yamano/Script/Tester.cs
+++ Assets/Yamano/Script/Tester.cs
@@ -7,16 +7,10 @@ namespace LucKee {
     //テスト用の適当なコンポーネント
     public class Tester : MonoBehaviour
     {
-        [SerializeField]
-        private ParticleSystem particle;
 
 
         private void Update()
         {
-            if (Input.anyKeyDown)
-            {
-                particle.Play();
-            }
         }
     }
 }
