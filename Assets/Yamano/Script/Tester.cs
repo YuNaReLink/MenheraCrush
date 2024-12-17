8d35d0c06da5c4cdc6af80eaecb7e66ddf565815
LucKee-the-Murderer <murderyy@gmail.com>
2024-12-17 17:01:18 +0900

    Numeric Sprite has been completed.

--- Assets/Yamano/Script/Tester.cs
+++ Assets/Yamano/Script/Tester.cs
@@ -7,21 +7,6 @@ namespace LucKee {
     //テスト用の適当なコンポーネント
     public class Tester : MonoBehaviour
     {
-        [SerializeField]
-        private Shaker shaker;
-
-        private void Awake()
-        {
-            shaker.Activate();
-        }
-        private void Update()
-        {
-            if (Input.anyKey)
-            {
-                shaker.Deactivate();
-            }
-        }
-
     }
 }
 
