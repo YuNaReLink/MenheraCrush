f32596566a5220b6c58928e670b67637967c2714
LucKee-the-Murderer <murderyy@gmail.com>
2025-01-10 16:32:36 +0900

    Ruby system has deleted because it can't write Japanese, and another was created.

--- Assets/Yamano/Outsiders/NamePlate.cs
+++ Assets/Yamano/Outsiders/NamePlate.cs
@@ -9,13 +9,16 @@ namespace LucKee
     [Serializable]
     public struct NamePlate
     {
-        private static readonly String Format = "<r={0}>{1}</r> <r={2}>{3}</r>";
-
         private String familyName;
         private String givenName;
         private String familyRuby;
         private String givenRuby;
+
+        public String FamilyName => familyName;
+        public String GivenName => givenName;
+        public String FamilyRuby => familyRuby;
+        public String GivenRuby => givenRuby;
+
         public String FullName => familyName + givenName;
-        public String RubiedFullName => String.Format(Format, familyRuby, familyName, givenRuby, givenName);
     }
 }
