2f7dd8e0d5bb87316e171cb73e8f02673c840f20
LucKee-the-Murderer <murderyy@gmail.com>
2025-01-10 14:29:21 +0900

    AnimParamChanger changes animator's "Number".

---
+++ Assets/Yamano/Outsiders/Animator/AnimParamChanger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace LucKee
+{
+    [RequireComponent(typeof(Animator))]
+    public class AnimParamChanger : MonoBehaviour
+    {
+        [SerializeField]
+        CharacterNameList character;
+
+        private void Awake()
+        {
+            Animator animator = GetComponent<Animator>();
+            animator.SetInteger("Number", (int)character);
+            Destroy(this);
+        }
+    }
+}
